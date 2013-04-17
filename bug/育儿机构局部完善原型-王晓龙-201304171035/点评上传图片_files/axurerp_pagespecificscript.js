0aefa6bf7c08e77cf247abd180be60195bfcbc1e
jo_yang <jo@360yuer.com>
2013-04-17 10:59:35 +0800

    https://github.com/it-yang/FX-ITU.git

---
+++ bug/育儿机构局部完善原型-王晓龙-201304171035/点评上传图片_files/axurerp_pagespecificscript.js
@@ -0,0 +1,62 @@
+﻿for(var i = 0; i < 47; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+document.getElementById('u31_img').tabIndex = 0;
+
+u31.style.cursor = 'pointer';
+$axure.eventManager.click('u31', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u41','','none',500);
+
+}
+});
+gv_vAlignTable['u36'] = 'center';gv_vAlignTable['u17'] = 'center';gv_vAlignTable['u28'] = 'center';gv_vAlignTable['u8'] = 'center';gv_vAlignTable['u30'] = 'center';gv_vAlignTable['u6'] = 'center';gv_vAlignTable['u32'] = 'center';gv_vAlignTable['u15'] = 'center';gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u4'] = 'center';gv_vAlignTable['u38'] = 'center';gv_vAlignTable['u43'] = 'center';gv_vAlignTable['u40'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u26'] = 'top';gv_vAlignTable['u10'] = 'center';document.getElementById('u9_img').tabIndex = 0;
+
+u9.style.cursor = 'pointer';
+$axure.eventManager.click('u9', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u20','','none',500);
+
+}
+});
+gv_vAlignTable['u23'] = 'top';gv_vAlignTable['u25'] = 'center';gv_vAlignTable['u46'] = 'center';gv_vAlignTable['u2'] = 'top';document.getElementById('u18_img').tabIndex = 0;
+
+u18.style.cursor = 'pointer';
+$axure.eventManager.click('u18', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u11','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u19'] = 'center';document.getElementById('u5_img').tabIndex = 0;
+
+u5.style.cursor = 'pointer';
+$axure.eventManager.click('u5', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u11','','none',500);
+
+}
+});
+gv_vAlignTable['u22'] = 'center';document.getElementById('u33_img').tabIndex = 0;
+
+u33.style.cursor = 'pointer';
+$axure.eventManager.click('u33', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u44','','none',500);
+
+}
+});
+gv_vAlignTable['u34'] = 'center';
---
+++ bug/育儿机构局部完善原型-王晓龙-201304171035/育儿机构_files/axurerp_pagespecificscript.js
@@ -0,0 +1,117 @@
+﻿for(var i = 0; i < 124; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u21'] = 'top';gv_vAlignTable['u86'] = 'center';gv_vAlignTable['u102'] = 'center';gv_vAlignTable['u16'] = 'center';gv_vAlignTable['u46'] = 'top';document.getElementById('u76_img').tabIndex = 0;
+
+u76.style.cursor = 'pointer';
+$axure.eventManager.click('u76', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u88','','none',500);
+
+}
+});
+gv_vAlignTable['u77'] = 'center';gv_vAlignTable['u93'] = 'center';gv_vAlignTable['u38'] = 'top';document.getElementById('u32_img').tabIndex = 0;
+
+u32.style.cursor = 'pointer';
+$axure.eventManager.click('u32', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u51','','none',500);
+
+}
+});
+gv_vAlignTable['u23'] = 'top';gv_vAlignTable['u53'] = 'center';u87.tabIndex = 0;
+
+u87.style.cursor = 'pointer';
+$axure.eventManager.click('u87', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u84','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u87'] = 'top';gv_vAlignTable['u1'] = 'top';gv_vAlignTable['u119'] = 'top';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u66'] = 'top';u112.tabIndex = 0;
+
+u112.style.cursor = 'pointer';
+$axure.eventManager.click('u112', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u109','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u112'] = 'top';gv_vAlignTable['u115'] = 'center';gv_vAlignTable['u104'] = 'center';gv_vAlignTable['u30'] = 'top';gv_vAlignTable['u8'] = 'top';gv_vAlignTable['u122'] = 'center';gv_vAlignTable['u64'] = 'top';gv_vAlignTable['u100'] = 'center';gv_vAlignTable['u79'] = 'center';gv_vAlignTable['u81'] = 'center';gv_vAlignTable['u118'] = 'center';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u41'] = 'top';gv_vAlignTable['u108'] = 'center';gv_vAlignTable['u71'] = 'center';gv_vAlignTable['u45'] = 'center';gv_vAlignTable['u36'] = 'top';gv_vAlignTable['u75'] = 'center';gv_vAlignTable['u37'] = 'top';gv_vAlignTable['u2'] = 'top';gv_vAlignTable['u83'] = 'center';gv_vAlignTable['u95'] = 'center';gv_vAlignTable['u22'] = 'top';gv_vAlignTable['u43'] = 'center';gv_vAlignTable['u0'] = 'top';gv_vAlignTable['u3'] = 'top';gv_vAlignTable['u68'] = 'center';gv_vAlignTable['u90'] = 'center';gv_vAlignTable['u73'] = 'center';gv_vAlignTable['u20'] = 'center';gv_vAlignTable['u50'] = 'top';gv_vAlignTable['u106'] = 'center';gv_vAlignTable['u28'] = 'top';gv_vAlignTable['u24'] = 'top';gv_vAlignTable['u54'] = 'top';document.getElementById('u99_img').tabIndex = 0;
+
+u99.style.cursor = 'pointer';
+$axure.eventManager.click('u99', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u109','','none',500);
+
+}
+});
+gv_vAlignTable['u39'] = 'top';gv_vAlignTable['u111'] = 'center';gv_vAlignTable['u69'] = 'top';gv_vAlignTable['u96'] = 'top';gv_vAlignTable['u61'] = 'center';u91.tabIndex = 0;
+
+u91.style.cursor = 'pointer';
+$axure.eventManager.click('u91', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u88','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u91'] = 'top';gv_vAlignTable['u35'] = 'center';gv_vAlignTable['u26'] = 'top';gv_vAlignTable['u65'] = 'top';gv_vAlignTable['u56'] = 'center';u116.tabIndex = 0;
+
+u116.style.cursor = 'pointer';
+$axure.eventManager.click('u116', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u113','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u116'] = 'top';gv_vAlignTable['u123'] = 'top';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u9'] = 'top';gv_vAlignTable['u33'] = 'center';document.getElementById('u72_img').tabIndex = 0;
+
+u72.style.cursor = 'pointer';
+$axure.eventManager.click('u72', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u120','','none',500);
+
+}
+});
+gv_vAlignTable['u63'] = 'center';gv_vAlignTable['u18'] = 'center';gv_vAlignTable['u48'] = 'center';document.getElementById('u101_img').tabIndex = 0;
+
+u101.style.cursor = 'pointer';
+$axure.eventManager.click('u101', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u113','','none',500);
+
+}
+});
+gv_vAlignTable['u14'] = 'top';document.getElementById('u74_img').tabIndex = 0;
+
+u74.style.cursor = 'pointer';
+$axure.eventManager.click('u74', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u84','','none',500);
+
+}
+});
+gv_vAlignTable['u59'] = 'center';gv_vAlignTable['u98'] = 'center';
